c63571c67979086dd4cd69f7ca44e0929d8fcdb8
Ruslan Karachun <free.changer@gmail.com>
2017-06-16 15:16:17 +0000

    Added form

--- js/app.jsx
+++ js/app.jsx
@@ -3,6 +3,38 @@ var app = app || {};
 (function () {
   'use strict';
 
+  class NameForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: ''};
+
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+      alert('A name was submitted: ' + this.state.value);
+      event.preventDefault();
+    }
+
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      );
+    }
+  }
+
+
   class Toggle extends React.Component {
     constructor(props) {
       super(props);
@@ -31,6 +63,7 @@ var app = app || {};
       <div>
         <h1>Hello, world</h1>
         <Toggle />
+        <NameForm />
       </div>
     ),
     document.getElementById('root')
